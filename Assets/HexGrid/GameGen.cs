143573834c1774f2162d8fe0820b7f7fde12f6b4
cwahlfeldt <chriswahlfeldt@gmail.com>
2017-05-20 16:45:52 -0500

    cool

---
+++ Assets/HexGrid/GameGen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameGen : MonoBehaviour {
+
+	public Material hexMaterial; //Assigned in inspector
+	public Material lineMaterial; //Assigned in inspector
+
+	private void Start() {
+
+		var grid = this.gameObject.AddComponent<Grid>();
+
+		//Set grid settings
+		grid.mapShape = MapShape.Hexagon;
+		grid.mapWidth = 4;
+		grid.mapHeight = 5;
+		grid.hexOrientation = HexOrientation.Flat;
+		grid.hexRadius = 1;
+		grid.hexMaterial = hexMaterial;
+		grid.addColliders = true;
+		grid.drawOutlines = true;
+		grid.lineMaterial = lineMaterial;
+
+		//Gen Grid
+		grid.GenerateGrid();
+
+		var tiles = grid.Tiles;
+		var i = 0;
+
+		foreach(var tile in tiles) {
+			tile.Value.name = "myTile" + i;
+			tile.Value.MoveCost = 1;
+			tile.Value.gameObject.tag = "Tile";
+			var myNeighbors = grid.Neighbours (tile.Value.index)[0];
+			Debug.Log (tile.Value.name);
+			i++;
+		}
+	}
+
+	private void PlayerGen () {
+		
+	}
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public Transform target;
+	private AstarAI aStarAI;
+	private AILerp aILerp;
+
+	// Use this for initialization
+	void Start () {
+		aStarAI = GetComponent<AstarAI> ();
+		aILerp = GetComponent<AILerp> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Click ();
+	}
+
+	void Click () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit)) {
+				Transform destination = hit.transform.gameObject.transform;
+
+				Debug.Log (hit.transform.gameObject.transform);
+				//aStarAI.targetPosition = destination;
+				aILerp.target = destination;
+			}
+		}
+	}
+}
